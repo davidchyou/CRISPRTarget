525fbe8a2b448f5089c4a927041e11e8bfc49ba6
chyte02p <chyte02p@aoraki-login.otago.ac.nz>
2025-02-17 14:41:36 +1300

    Modified the testing script, making binaries executable

---
+++ test_run.sh
@@ -0,0 +1,26 @@
+#Make all binaries executable
+chmod 777 bedtools
+chmod 777 blastdbcmd
+chmod 777 blastn
+chmod 777 esl-shuffle
+chmod 777 makeblastdb
+chmod 777 samtools
+
+#Construct BLASTDB and the index file from FASTA and shuffle, then search for targets.
+perl CRISPRTarget.pl \
+-gff sample_crispr_gff/PSA.crispr.gff \
+-user_fasta sample_db/vhdb_selected.fna \
+-dbsize 100000000 \
+-evalue 1 \
+-out test_out \
+-pam_search_all
+
+#Search for targets from precomputed BLASTDB and the index file.
+perl CRISPRTarget.pl \
+-gff sample_crispr_gff/PSA.crispr.gff \
+-db USER_DB/vhdb_selected.fna \
+-ctrl_db USER_SHUFFLED_DB/vhdb_selected.fna \
+-dbsize 100000000 \
+-evalue 1 \
+-out test_out_2 \
+-pam_search_all
