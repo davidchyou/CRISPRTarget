a62d45dfeed0131e01d6e46865e039c167f58cc5
ChrisBrownNZ <30485971+ChrisBrownNZ@users.noreply.github.com>
2026-09-15 09:04:05 +1200

    Update run_setup_db.sh

--- run_setup_db.sh
+++ run_setup_db.sh
@@ -13,12 +13,23 @@ cd ../
 #Construct BLASTDB and the index file from FASTA and shuffle, then search for targets.
 perl CRISPRTarget.pl \
 -gff sample_crispr_gff/PSA.crispr.gff \
+-db USER_DB/vhdb_selected.fna \
+-ctrl_db USER_SHUFFLED_DB/vhdb_selected.fna \
+-dbsize 100000000 \
+-evalue 1 \
+-out test_out_vhdb \
+-pam_search_all \
+-make_user_db
+
+perl CRISPRTarget.pl \
+-gff sample_crispr_gff/PSA.crispr.gff \
 -user_fasta DB/phage.fa \
 -dbsize 100000000 \
 -evalue 1 \
--out test_out_1 \
--pam_search_all
+-out test_out_phage \
+-pam_search_all \
 -make_user_db
+
 
 #Search for targets from precomputed BLASTDB and the index file.
 perl CRISPRTarget.pl \
@@ -27,5 +38,6 @@ perl CRISPRTarget.pl \
 -ctrl_db USER_SHUFFLED_DB/phage.fa \
 -dbsize 100000000 \
 -evalue 1 \
--out test_out_2 \
+-out test_out_phage_run \
+-keep_user_db \
 -pam_search_all
